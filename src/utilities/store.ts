6e19926c2791fadd8c1cc007077088163418269a
Pato <patobottos@gmail.com>
2024-01-10 15:34:50 +0100

    Installing Zustand

--- src/utilities/store.ts
+++ src/utilities/store.ts
@@ -1,2 +1,31 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import { computeGuess, getRandomWordEng, LetterState } from './word-utils';
+
 export const WORD_LENGTH = 5;
 export const NUMBER_OF_GUESSES = 6;
+interface StoreState {
+  answerWord: string;
+  guesses: string[];
+  addGuess: (guess: string) => void;
+
+}
+
+
+export const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      answerWord: getRandomWordEng(),
+      guesses: ['close', 'local', 'claim'],
+      addGuess: (guess: string) => {
+        set(state => ({
+          guesses: [...state.guesses, guess]
+        }))
+      }
+    }),
+    {
+      name: "wordle",
+    },
+
+  )
+);
