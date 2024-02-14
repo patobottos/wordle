cea88a58e67a360e99be7659bac4437fbb61f5fb
Pato <patobottos@gmail.com>
2024-02-14 01:27:07 +0100

    Adjusting footer for small screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,32 @@
 function Footer() {
   return (
-    <div className="flex flex-row justify-center mx-4 mt-8 mb-4 border-t-2 w-[90vw] text-xs pt-4 space-x-2 text-gray-700">
-      <p className="cursor-pointer text-blue-800">
+    <div className="flex flex-row justify-center mx-2 mt-8 mb-4 border-t-2 w-[94vw] text-xs pt-4 text-gray-700 xxs:mx-0.5 xs:mx-0.5 xxs:text-xx">
+      <span className="mx-2 xxs:mx-1 xs:mx-1">
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://github.com/patobottos"
+          href="https://patobottos.vercel.app/"
+          className="decoration-inherit text-blue-800 cursor-pointer"
         >
           @Pato Bottos
         </a>
-      </p>
-      <p>|</p>
-      <p className="cursor-pointer">
+      </span>
+      <span>|</span>
+      <span className="mx-2 xxs:mx-1 xs:mx-1">
         Inspired by:{" "}
         <a
           target="_blank"
           rel="noreferrer"
           href="https://hswolff.com/"
-          className="text-blue-800"
+          className="decoration-inherit text-blue-800 cursor-pointer"
         >
           Harry Wolff
         </a>
-      </p>
-      <p>|</p>
-      <p>Barcelona, 2024</p>
+      </span>
+      <span>|</span>
+      <span className="mx-2 xxs:mx-1 xs:mx-1 text-gray-700 cursor-pointer">
+        Barcelona, 2024
+      </span>
     </div>
   );
 }
